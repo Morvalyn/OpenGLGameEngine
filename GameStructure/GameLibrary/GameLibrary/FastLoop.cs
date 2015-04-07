29cd009a8b4aa46aff7460e131fe8f4b7232a189
Nolan Bradshaw <nolanjbradshaw@gmail.com>
2015-04-06 17:40:09 -0700

    Added Library Folder

---
+++ GameStructure/GameLibrary/GameLibrary/FastLoop.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+
+namespace GameLibrary
+{
+    public struct Message
+    {
+        public IntPtr hWnd;
+        public Int32 msg;
+        public IntPtr wParam;
+        public IntPtr lParam;
+        public uint time;
+        public System.Drawing.Point p;
+    }
+
+    public class FastLoop
+    {
+        [System.Security.SuppressUnmanagedCodeSecurity]
+        [DllImport("User32.dll", CharSet = CharSet.Auto)]
+        public static extern bool PeekMessage(out Message msg, IntPtr hWnd, uint messageFilterMin, uint messageFilterMax, uint flags);
+
+        PreciseTimer _timer = new PreciseTimer();
+        public delegate void LoopCallback(double elapsedTime);
+        LoopCallback _callback;
+        public FastLoop(LoopCallback callback)
+        {
+            _callback = callback;
+            Application.Idle += new EventHandler(OnApplicationEnterIdle);
+        }
+
+        private void OnApplicationEnterIdle(object sender, EventArgs e)
+        {
+            while (IsAppStillIdle())
+            {
+                _callback(_timer.GetElapsedTime());
+            }
+        }
+
+        private bool IsAppStillIdle()
+        {
+            Message msg;
+            return !PeekMessage(out msg, IntPtr.Zero, 0, 0, 0);
+        }
+    }
+}
